4dc81cd4307c3247b191dc0c3ce8063b283efad5
John Fabiszewski <kumolanding@gmail.com>
2023-11-07 11:02:44 -0600

    31 axios switch, no proxy

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,3 +1,4 @@
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL = "https://fabian-project1.onrender.com:3001";
+// axios.defaults.baseURL = "http://localhost:3001";
 export default axios;
